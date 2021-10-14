397e0f5e01ee0921a05f9b8c57fe1b4e8064ca6c
KangInhye <inhye@ramdel.kr>
2021-10-14 18:15:15 +0900

    page-why card css

---
+++ src/components/Card-posts.js
@@ -0,0 +1,25 @@
+const posts = [
+  {
+    title: "My first post",
+    Content: "This is my first post with more content inside",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "My second post",
+    Content: "This is my second post with more content inside",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "My third post",
+    Content: "This is my third post with more content inside",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+
+  {
+    title: "My fourth post",
+    Content: "This is my fourth post with more content inside",
+    image: "https://bit.ly/2WNi2Ml",
+  },
+];
--- src/pages/Why.js
+++ src/pages/Why.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { makeStyles, Typography, Card } from "@material-ui/core";
+import Button from "@mui/material/Button";
+import CheckIcon from "@mui/icons-material/Check";
 
 const useStyles = makeStyles((theme) => ({
   customCard: {
@@ -8,6 +10,8 @@ const useStyles = makeStyles((theme) => ({
     height: "327px",
     background: "#fff",
     borderRadius: "8px",
+    display: "inline-block",
+    marginLeft: theme.spacing(5),
   },
 }));
 
@@ -17,21 +21,31 @@ function Why() {
   return (
     <div className="page-why">
       <h1>Why FlutterUI?</h1>
-      <Card className={classes.customCard} variant="outlined">
-        <div className="upper-container">
-          <div className="image-container">
-            <img src="" alt="" height="100px" width="100px" />
+      <div className="card-position">
+        <p style={{ textAlign: "start" }}>Top Features</p>
+        <Card className={classes.customCard} variant="outlined">
+          <div className="upper-container">
+            <div className="image-container">
+              <img src="free.png" alt="" />
+            </div>
           </div>
-        </div>
-        <div className="lower-container">
-          <h6>Free and Open Source</h6>
-          <p>
-            Use FlutterUI free of charge for both personal and commercial
-            projects.
-          </p>
-        </div>
-        <button></button>
-      </Card>
+          <div className="lower-container">
+            <h6>Free and Open Source</h6>
+            <p>
+              Use FlutterUI free of charge for both personal and commercial
+              projects.
+            </p>
+          </div>
+          <Button
+            startIcon={<CheckIcon />}
+            style={{ textTransform: "none" }}
+            variant="contained"
+            disabled
+          >
+            Included
+          </Button>
+        </Card>
+      </div>
     </div>
   );
 }
